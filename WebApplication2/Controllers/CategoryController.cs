21ac020a0ec94385dd6d65d0b5ef5ff189507d6d
diego cascavita <sportscenter857@gmail.com>
2023-06-25 11:06:05 -0500

    added(add category view)

--- WebApplication2/Controllers/CategoryController.cs
+++ WebApplication2/Controllers/CategoryController.cs
@@ -1,10 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Data;
+using WebApplication2.Models;
 
 namespace WebApplication2.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db) { 
+            _db = db;
+        }
         public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+        public IActionResult Create()
         {
             return View();
         }
